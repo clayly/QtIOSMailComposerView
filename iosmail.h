f83847cb7106f2f0dd6c3588e4f79fc852414c66
clayly <31773799+clayly@users.noreply.github.com>
2019-02-11 16:12:33 +0300

    Update iosmail.h

--- iosmail.h
+++ iosmail.h
@@ -3,7 +3,6 @@
 #include <QObject>
 
 Q_FORWARD_DECLARE_OBJC_CLASS(IOSMailDelegate);
-//Q_FORWARD_DECLARE_OBJC_CLASS(MFMailComposeViewController);
 
 class IOSMail : public QObject
 {
@@ -16,6 +15,5 @@ public:
     Q_INVOKABLE void contactUs();
 
 private:
-    IOSMailDelegate* m_delegate;
-//    __strong MFMailComposeViewController* m_controller;
+    IOSMailDelegate* const m_delegate;
 };
